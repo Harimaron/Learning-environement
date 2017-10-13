fa7078075d4b760cacf0d9cc70cba294abf3195e
Harimaron <gaetan.ribonnet@outlook.com>
2017-10-13 16:40:43 +0200

    pendu

---
+++ 06-JEUX_PENDU/pendu.js
@@ -0,0 +1,38 @@
+
+
+
+function guessLetter("B", "O", "N", "J", "O", "U", "R"); {
+    for (var i = 0; i < myArray.length; i++);
+    var CorrectLetter = prompt("B")
+
+
+}
+
+var myArray = ["B", "O", "N", "J", "O", "U", "R"];
+var myArrayTwo = ["", "", "", "", "", "", "_"];
+for (var i = 0; i < myArray.length; i++) {
+    var  = myArray[i];
+
+}
+
+
+
+
+/* Créer deux tableaux, un pour les lettres du mot et un pour les lettres non trouvées. */
+var trueLetter = ["B", "O", "N", "J", "O", "U", "R"];
+var missingLetter = ["", "", "", "", "", "", "_"];
+/* Créer une fonction guessLetter qui contiendra chaque lettres du tableau. */
+/* Vérifier à chaque lettre proposée par l'utilisateur si elle est dans le mot. */
+function guessLetter(trueLetter) {
+
+
+}
+
+
+for (var i = 0; i < trueLetter.length; i++); {
+    if (i == 0) {
+        console.log(i + ' est impaire');
+    } else {
+        console.log(i + ' est paire');
+    }
+}
